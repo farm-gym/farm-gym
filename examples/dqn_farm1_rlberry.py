e5e5d6c45eb0bcb89190729bbb15cbebb19da804
Timothee Mathieu <timothee.mathieu@inria.fr>
2022-12-21 15:06:39 +0100

    add ppo and dqn xps

--- examples/dqn_farm1_rlberry.py
+++ examples/dqn_farm1_rlberry.py
@@ -20,6 +20,8 @@ from rlberry.agents.torch.utils.training import model_factory_from_env
 import pandas as pd
 import seaborn as sns
 import time
+import matplotlib.pyplot as plt
+
 
 actions_txt = ["doing nothing",
                "1L of water",
@@ -48,29 +50,36 @@ if __name__ == "__main__":
         parallelization="process",
         mp_context="spawn",
         enable_tensorboard=True,
+        seed = 42
     )
 
     init_time = time.time()
     manager.fit()
-    print("training time in s is ", time.time()-init_time())
+    print("training time in s is ", time.time()-init_time)
     data = plot_writer_data(manager, tag="episode_rewards", smooth_weight=0.8) # smoothing tensorboard-style
 
     plt.savefig('dqn_regret.pdf')
 
     agent = manager.agent_handlers[0] # select the agent from the manager
-    env = Farm1()
-    obs = env.reset()
+    env = gym_make('Farm1-v0')
 
+    rew = 0
+    while rew < 1:
+        rew = 0
+        obs = env.reset()
+        episode = pd.DataFrame()
+        for day in range(365):
+            action = agent.policy(obs)
+            obs, reward, is_done,_ =  env.step(action)
+            episode = pd.concat([episode, pd.DataFrame({'action':[actions_txt[action]],
+                                                        'reward':[reward]})], ignore_index=True)
+            rew = rew + reward
+            if is_done:
+                print('Plant is Dead')
+                break
 
-    episode = pd.DataFrame()
-    for day in range(365):
-        action = agent.policy(obs)
-        obs,reward, is_done,_ =  env.step(action)
-        episode = pd.concat([episode, pd.DataFrame({'action':[actions_txt[action]],
-                                                    'reward':[reward]})], ignore_index=True)
-        if is_done:
-            print('Plant is Dead')
-            break
+        print(rew)
+        
     fig, ax = plt.subplots(figsize=(12,6))
-    sns.countplot(data = episode, x = "action", ax = ax)
-    fig.savefig('dqn_boxplot.pdf')
+    sns.countplot(data = episode, x = "action",order = episode['action'].value_counts().index,  ax = ax)
+    fig.savefig('dqn_barplot.pdf')
--- examples/ppo_farm1_rlberry.py
+++ examples/ppo_farm1_rlberry.py
@@ -20,6 +20,7 @@ from rlberry.agents.torch.utils.training import model_factory_from_env
 import pandas as pd
 import seaborn as sns
 import time
+import matplotlib.pyplot as plt
 
 policy_configs = {
     "type": "MultiLayerPerceptron",  # A network architecture
@@ -61,38 +62,46 @@ if __name__ == "__main__":
             learning_rate=9e-5,
             n_steps=5 * 365,
             batch_size=365,
-            eps_clip=0.2,
+            eps_clip=0.1,
         ),
-        fit_budget=5e4,
+        fit_budget=5e5,
         eval_kwargs=dict(eval_horizon=365),
         n_fit=1,
         parallelization="process",
         mp_context="spawn",
         enable_tensorboard=True,
+        seed = 42
     )
     
     init_time = time.time()
     manager.fit()
-    print("training time in s is ", time.time()-init_time())
+    print("training time in s is ", time.time()-init_time)
     
     data = plot_writer_data(manager, tag="episode_rewards", smooth_weight=0.8) # smoothing tensorboard-style
 
     plt.savefig('ppo_regret.pdf')
 
     agent = manager.agent_handlers[0] # select the agent from the manager
-    env = Farm1()
-    obs = env.reset()
+    env = gym_make('Farm1-v0')
+    
 
+    rew = 0
+    while rew < 1:
+        rew = 0
+        obs = env.reset()
+        episode = pd.DataFrame()
+        for day in range(365):
+            action = agent.policy(obs)
+            obs, reward, is_done,_ =  env.step(action)
+            episode = pd.concat([episode, pd.DataFrame({'action':[actions_txt[action]],
+                                                        'reward':[reward]})], ignore_index=True)
+            rew = rew + reward
+            if is_done:
+                print('Plant is Dead')
+                break
 
-    episode = pd.DataFrame()
-    for day in range(365):
-        action = agent.policy(obs)
-        obs,reward, is_done,_ =  env.step(action)
-        episode = pd.concat([episode, pd.DataFrame({'action':[actions_txt[action]],
-                                                    'reward':[reward]})], ignore_index=True)
-        if is_done:
-            print('Plant is Dead')
-            break
+        print(rew)
+        
     fig, ax = plt.subplots(figsize=(12,6))
-    sns.countplot(data = episode, x = "action", ax = ax)
-    fig.savefig('ppo_boxplot.pdf')
+    sns.countplot(data = episode, x = "action",order = episode['action'].value_counts().index,  ax = ax)
+    fig.savefig('ppo_barplot.pdf')
